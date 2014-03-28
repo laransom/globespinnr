3ddcdc1cee232b2c10779b305cf5ea7b6cee85f6
laransom <laransom@gmail.com>
2014-03-28 11:21:35 -0400

    Blank rails with postgres minus test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Globespinnr::Application.config.secret_key_base = 'f0ca5d205e19883764afef7988a3e3ca2fe13772908f19e00310ca6d6c8b11f06c15b75ee21fadc17e6175c89235a341ada59417dd6bca816a8546b8b9cdce36'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Globespinnr::Application.config.session_store :cookie_store, key: '_globespinnr_session'
