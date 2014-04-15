fc3d8d3004b0e871db81b871db6365579c46ead5
laransom <laransom@gmail.com>
2014-04-15 18:18:37 -0400

    set up namespace in routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,10 @@ Globespinnr::Application.routes.draw do
   end
 
   resources :likes, only: [:create]
+
+  resources :admin do
+    resources :photos, only: [:destroy]
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
