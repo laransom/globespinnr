c8500b663dfad38a8562392f9026f008a593a0d7
laransom <laransom@gmail.com>
2014-04-15 17:23:47 -0400

    added method tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,15 @@
 require 'spec_helper'
 
 describe User do
+
+  let(:user) { FactoryGirl.create(:user)}
+  let(:photo_1) { FactoryGirl.create(:photo) }
+  let(:photo_2) { FactoryGirl.create(:photo) }
+  let(:photo_3) { FactoryGirl.create(:photo) }
+  let(:photo_4) { FactoryGirl.create(:photo) }
+  let(:photo_5) { FactoryGirl.create(:photo) }
+
+
   it { should have_valid(:first_name).when('George', 'Pete') }
   it { should_not have_valid(:first_name).when(nil, '') }
 
@@ -12,4 +21,9 @@ describe User do
 
   it { should have_many(:photos) }
   it { should have_many(:comments) }
+
+  it 'returns first_name' do
+    expect(user.full_name).to eq('Cyril Figgus')
+  end
+
 end
