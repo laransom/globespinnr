b6fd8fc08eeea27fa3c689aee98e98a70f61359e
laransom <laransom@gmail.com>
2014-04-15 16:00:01 -0400

    last test passing

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,6 +3,6 @@ class Like < ActiveRecord::Base
   validates_inclusion_of :score, in: [1]
 
   belongs_to :user
-  belongs_to :photo, counter_cache: :count_of_likes
+  belongs_to :photo, counter_cache: true
 
 end
