20ed84f36a46a989f58a92c11f2a87bef47f9440
laransom <laransom@gmail.com>
2014-04-15 17:29:02 -0400

    removed some factory girls

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,12 +3,6 @@ require 'spec_helper'
 describe User do
 
   let(:user) { FactoryGirl.create(:user)}
-  let(:photo_1) { FactoryGirl.create(:photo) }
-  let(:photo_2) { FactoryGirl.create(:photo) }
-  let(:photo_3) { FactoryGirl.create(:photo) }
-  let(:photo_4) { FactoryGirl.create(:photo) }
-  let(:photo_5) { FactoryGirl.create(:photo) }
-
 
   it { should have_valid(:first_name).when('George', 'Pete') }
   it { should_not have_valid(:first_name).when(nil, '') }
