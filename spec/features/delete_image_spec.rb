e6341665124703f812cf4a319a45d6801433511e
laransom <laransom@gmail.com>
2014-04-16 10:32:59 -0400

    fixed authorized delete spec

--- spec/features/delete_image_spec.rb
+++ spec/features/delete_image_spec.rb
@@ -5,17 +5,25 @@ feature 'delete photo from globespinnr', %q{
   I want to be able to delete a photo I uploaded
   So that it won't be visible to other users
 } do
-  scenario 'delete photo' do
-    photo = FactoryGirl.create(:photo)
-    photo_count = Photo.count
-    visit location_path(photo.location)
+  context 'as an authenticated user' do
 
-    within "#photos" do
-      first(:link, 'a').click
+    let(:photo) { FactoryGirl.create(:photo) }
+
+    before :each do
+      sign_in_as(photo.user)
     end
 
-    click_on 'Delete Photo'
+    scenario 'delete photo' do
+      photo_count = Photo.count
+      visit location_path(photo.location)
 
-    expect(Photo.count).to eq(photo_count -1)
+      within "#photos" do
+        first(:link, 'a').click
+      end
+
+      click_on 'Delete Photo'
+
+      expect(Photo.count).to eq(photo_count - 1)
+    end
   end
 end
