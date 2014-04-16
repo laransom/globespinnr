cb30aaa3bb2bd68d0e3a2e99a6333139520f4a84
laransom <laransom@gmail.com>
2014-04-16 10:27:51 -0400

    fixed likes so only one like per user per photo

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,12 +3,13 @@ class LikesController < ApplicationController
   before_filter :authenticate_user!, only: [:create, :new]
 
   def create
-    @like = Like.new(like_params)
+    @like ||= Like.new(like_params)
     @like.user = current_user
 
     if @like.save
       redirect_to photo_path(@like.photo), notice: 'Liked'
     else
+      redirect_to photo_path(@like.photo), alert: 'Already liked'
     end
 
   end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,7 @@
 class Like < ActiveRecord::Base
 
   validates_inclusion_of :score, in: [1]
+  validates_uniqueness_of :user_id, scope: :photo_id
 
   belongs_to :user
   belongs_to :photo, counter_cache: true
