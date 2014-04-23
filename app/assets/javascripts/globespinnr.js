940504fcee7541174a7a4337402964922dfeb489
laransom <laransom@gmail.com>
2014-04-23 11:53:57 -0400

    deleted unused javascript

--- app/assets/javascripts/globespinnr.js
+++ app/assets/javascripts/globespinnr.js
@@ -1,23 +1,9 @@
-$(document).ready(function() {
-  var str = '';
-  $('#country').change(function() {
-    $('select option:selected').each(function() {
-      str = $(this).val();
-    });
-  });
-
-
-  $('#submit').click(function() {
-    var path = $('#country').val();
-    window.location.assign("/locations/" + str);
-  });
-});
-
 function fixDropdown() {
     var maxheight = $(window).height() - 20;
     var dropdown = $('ul.dropdown');
     dropdown.css({ 'max-height': maxheight });
     console.log('called fix dropdown');
-};
+}
+
 $(window).on('load', fixDropdown);
 $(window).on('resize', fixDropdown);
