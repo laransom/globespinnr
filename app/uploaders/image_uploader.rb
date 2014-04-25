9453429df6ec202157a33824b41374bf34c13c15
laransom <laransom@gmail.com>
2014-04-25 12:13:56 -0400

    default profile on amazon

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -25,9 +25,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
-    ActionController::Base.helpers.asset_path("fallback/" + [version_name, "silhouette.jpg"].compact.join('_'))
+    # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "silhouette.jpg"].compact.join('_'))
     # "/assets/images/fallback/silhouette.jpg"
     # "/images/fallback/" + [version_name, "silhouette.jpg"].compact.join('_')
+    "https://s3.amazonaws.com/globespinnr-image-development/" + [version_name, 'silhouette.jpg'].compact.join('_')
+
   end
 
   # Process files as they are uploaded:
