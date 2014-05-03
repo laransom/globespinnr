e2c944677cfede214ee3d7af87674292ab04af30
laransom <laransom@gmail.com>
2014-05-03 15:51:31 -0400

    removed threex script from assets

--- app/assets/javascripts/globespinnr.js
+++ app/assets/javascripts/globespinnr.js
@@ -7,24 +7,3 @@ function fixDropdown() {
 $(window).on('load', fixDropdown);
 $(window).on('resize', fixDropdown);
 
-
-var THREEx  = THREEx    || {};
-
-THREEx.WindowResize = function(renderer, camera){
-  var callback  = function(){
-
-    renderer.setSize( window.innerWidth, window.innerHeight );
-
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-  };
-
-  window.addEventListener('resize', callback, false);
-
-  return {
-
-    stop  : function(){
-      window.removeEventListener('resize', callback);
-    }
-  };
-};
